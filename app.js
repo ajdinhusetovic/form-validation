0ac1832b94b9a06e1310ccc4d2455d2a2cc71914
Ajdin <ajdinhusetovic@outlook.com>
2023-01-21 16:10:06 +0100

    created basic form, added the form element selectors in js

---
+++ app.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("form");
+const email = document.getElementById("mail");
+const country = document.getElementById("country");
+const zipCode = document.getElementById("zip-code");
+const password = document.getElementById("password");
+const passwordConfirm = document.getElementById("password-confirmation");
+
+// errors
+const error = document.querySelector("span.error");
